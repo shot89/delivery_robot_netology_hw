9062df10dadc740b032aa5b67ac9c6ef98a36b32
Shot <shot89@gmail.com>
2023-03-15 16:21:28 +0300

    Ex 2

--- src/main/java/ru/netology/Main.java
+++ src/main/java/ru/netology/Main.java
@@ -10,7 +10,29 @@ public class Main {
     public static char NEED_COUNT_CHAR = 'R';
     public static final Map<Integer, Integer> sizeToFreq = new HashMap<>();
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+
+        Thread printThread = new Thread(
+                () -> {
+                    int max = 0;
+                    while (!Thread.interrupted()) {
+                        synchronized (sizeToFreq) {
+                            try {
+                                sizeToFreq.wait();
+                                int iterMax = sizeToFreq.keySet().stream().mapToInt(v -> v).max().orElseThrow(NoSuchElementException::new);
+                                if (iterMax > max) {
+                                    System.out.println("Новый максимум вхождений 'R' в команду робота: " + iterMax);
+                                    max = iterMax;
+                                }
+                            } catch (InterruptedException e) {
+                                return;
+                            }
+                        }
+                    }
+                }
+        );
+
+        printThread.start();
 
         for (int i = 0; i < THREAD_COUNT; i++) {
             new Thread(
@@ -22,11 +44,14 @@ public class Main {
                                 sizeToFreq.put(count, sizeToFreq.get(count) + 1);
                             } else {
                                 sizeToFreq.put(count, 1);
+                                sizeToFreq.notify();
                             }
                         }
                     }
             ).start();
         }
+
+        printThread.interrupt();
 
         List<Map.Entry<Integer, Integer>> result = sizeToFreq.entrySet().stream()
                 .sorted(Map.Entry.<Integer, Integer>comparingByValue().reversed())
@@ -34,7 +59,8 @@ public class Main {
 
         Iterator<Map.Entry<Integer, Integer>> iterator = result.iterator();
         Map.Entry<Integer, Integer> entry = iterator.next();
-        System.out.println("Самое частое количество повторений " + result.get(0).getKey() + " (встретилось " + result.get(0).getValue() + " раз)");
+        System.out.println("Самое частое количество повторений " + result.get(0).getKey() +
+                " (встретилось " + result.get(0).getValue() + " раз)");
 
         System.out.println("Другие размеры:");
         while (iterator.hasNext()) {
